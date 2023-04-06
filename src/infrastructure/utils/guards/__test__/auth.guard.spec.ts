692cadddb48d96ea802d2e67731df7bb1db689a1
Julian362 <Juliangarcia362@hotmail.com>
2023-04-05 22:12:22 -0500

    se agregan guard
    guard implementado con token de usuario

---
+++ src/infrastructure/utils/guards/__test__/auth.guard.spec.ts
@@ -0,0 +1,64 @@
+import { ExecutionContext } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from '@patient/infrastructure/services/secret';
+import { of } from 'rxjs';
+import { AuthGuard } from '../auth.guard';
+
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
+  let mockContext: ExecutionContext;
+  let mockJwtService: JwtService;
+
+  beforeEach(() => {
+    mockJwtService = {
+      verify: jest.fn(),
+    } as unknown as JwtService;
+
+    mockContext = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getRequest: jest.fn().mockReturnValue({
+        headers: {
+          login: 'Bearer token',
+        },
+      }),
+    } as unknown as ExecutionContext;
+
+    guard = new AuthGuard(mockJwtService);
+  });
+
+  describe('canActivate', () => {
+    it('should return true if the token is verified', async () => {
+      // Arrange
+      const mockVerifyResult = true;
+      (mockJwtService.verify as jest.Mock).mockReturnValue(
+        of(mockVerifyResult),
+      );
+
+      // Act
+      const result = await guard.canActivate(mockContext).toPromise();
+
+      // Assert
+      expect(result).toBe(mockVerifyResult);
+      expect(mockJwtService.verify).toHaveBeenCalledWith('Bearer', {
+        secret: jwtConstants.secret,
+      });
+    });
+
+    it('should return false if the token is not verified', async () => {
+      // Arrange
+      const mockVerifyResult = false;
+      (mockJwtService.verify as jest.Mock).mockReturnValue(
+        of(mockVerifyResult),
+      );
+
+      // Act
+      const result = await guard.canActivate(mockContext).toPromise();
+
+      // Assert
+      expect(result).toBe(mockVerifyResult);
+      expect(mockJwtService.verify).toHaveBeenCalledWith('Bearer', {
+        secret: jwtConstants.secret,
+      });
+    });
+  });
+});
---
+++ src/infrastructure/utils/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from '@patient/infrastructure/services/secret';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  canActivate(context: ExecutionContext): Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers.login;
+    const token = authHeader.split(' ')[0];
+    return this.jwtService.verify(token, {
+      secret: jwtConstants.secret,
+    });
+  }
+}
