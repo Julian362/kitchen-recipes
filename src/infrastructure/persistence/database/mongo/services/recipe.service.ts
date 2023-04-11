b53367f756e64dbdcb109d8ee7f63c9c451d5142
Julian362 <Juliangarcia362@hotmail.com>
2023-04-11 11:19:27 -0500

    se modifica servicio
    se modifica el tipado de la respuesta de los metodos del servicio

--- src/infrastructure/persistence/database/mongo/services/recipe.service.ts
+++ src/infrastructure/persistence/database/mongo/services/recipe.service.ts
@@ -1,26 +1,25 @@
-import { RecipeDomainModel } from '@domain/models';
 import { IRecipeService } from '@domain/services/recipes.service';
+import { RecipeMongo } from '@infrastructure/persistence';
 import { RecipeRepository } from '@infrastructure/persistence/database/mongo/repositories/recipe.repository';
-import { RecipeMongo } from '@infrastructure/persistence/database/mongo/schemas/recipe.schema';
 import { Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 @Injectable()
 export class RecipeMongoService implements IRecipeService {
   constructor(private readonly recipeRepository: RecipeRepository) {}
-  create(entity: RecipeDomainModel): Observable<RecipeMongo> {
+  create(entity: RecipeMongo): Observable<RecipeMongo> {
     return this.recipeRepository.create(entity);
   }
-  findById(id: string): Observable<RecipeDomainModel> {
+  findById(id: string): Observable<RecipeMongo> {
     return this.recipeRepository.findById(id);
   }
-  update(id: string, recipe: RecipeDomainModel): Observable<RecipeDomainModel> {
+  update(id: string, recipe: RecipeMongo): Observable<RecipeMongo> {
     return this.recipeRepository.update(id, recipe);
   }
-  delete(id: string): Observable<RecipeDomainModel> {
+  delete(id: string): Observable<RecipeMongo> {
     return this.recipeRepository.delete(id);
   }
 
-  findAllByUserId(id: string): Observable<RecipeDomainModel[]> {
+  findAllByUserId(id: string): Observable<RecipeMongo[]> {
     return this.recipeRepository.findAllByUserId(id);
   }
 }
